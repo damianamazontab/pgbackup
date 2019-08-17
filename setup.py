01a533bc3acaa83d52c73aa1877766a04d0175fd
damianamazontab <49444322+damianamazontab@users.noreply.github.com>
2019-08-17 12:39:23 +0100

    adding setup.py and README

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages, setup
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='pgbackup',
+    version='0.1.0',
+    author='Keith Thomson',
+    description = 'A utility for backing up PostgreSQL databases.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/linuxacademy/pgbackup',
+    packages=find_packages('src')
+)
